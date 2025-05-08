4e5d22665f52276d0826a0471de6f42a75b39829
votrongluan <trongluan115@gmail.com>
2025-05-09 01:12:31 +0700

    update: change backend api

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const publicRuntimeConfig = {
-  // API_URL: "https://sp25se026gsp48wdcrbpapi-production.up.railway.app",
-  API_URL: "http://10.0.2.2:8080",
+  API_URL: "https://springboot-app-846303142303.asia-southeast1.run.app",
+  // API_URL: "http://10.0.2.2:8080",
 };
 
 export const { API_URL } = publicRuntimeConfig;
